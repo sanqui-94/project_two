db063a054dce6fa82d7bbb6926f323b3827200f2
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2023-09-15 11:36:25 -0400

    adding maps alternatives

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use strict";
-console.log("vaya a dormir");
+Object.defineProperty(exports, "__esModule", { value: true });
+const form = document.querySelector("form");
+function searchAddressHandler(event) {
+    event.preventDefault();
+    const coordinates = { lat: 40.41, lng: -73.99 };
+    document.getElementById('map').innerHTML = '';
+    new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+            zoom: 16
+        })
+    });
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", searchAddressHandler);
 //# sourceMappingURL=app.js.map
